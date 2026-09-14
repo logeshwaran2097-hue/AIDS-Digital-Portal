39dcb32150863137ac8ef0cb1edce581abdf1b91
logeshwaran2097-hue <logeshwaran2097@gmail.com>
2026-09-14 20:58:32 +0530

    chore: remove unused files, dead scripts, stale sqlite dbs, duplicate apks, and unreferenced components

--- src/components/AIAssistant.tsx
+++
@@ -1,4 +0,0 @@
-'use client'
-
-export { AIChatbot as AIAssistant, AIChatbot } from './AIChatbot'
-export { AIChatbot as default } from './AIChatbot'
--- src/lib/projectData.ts
+++
@@ -1,1 +0,0 @@
-export const FALLBACK_PROJECTS: any[] = []
